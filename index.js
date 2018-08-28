2d26c9a9fc416fb7b995d0748e929afc426191cb
Hoa Luu <hoamluu@gmail.com>
2018-08-28 11:57:40 -0400

    gpio18

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const board = new five.Board({
 board.on("ready", function() {
 
   // Create a new `button` hardware instance.
-  var button = new five.Button("P1-7");
+  var button = new five.Button("P1-12");
 
   button.on("hold", function() {
     console.log( "Button held" );
