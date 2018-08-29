09902f4ba4994e9d214342245d8d0e3e18d13f20
Hoa Luu <hoamluu@gmail.com>
2018-08-29 14:59:50 -0400

    onOff

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const { StillCamera } = require('pi-camera-connect');
-const stillCamera = new StillCamera();
-const fs = require('fs');
-const fetch = require('node-fetch');
+// const { StillCamera } = require('pi-camera-connect');
+// const stillCamera = new StillCamera();
+// const fs = require('fs');
+// const fetch = require('node-fetch');
 
 
-const raspi = require('raspi-io');
-const FormData = require('form-data');
-const five = require('johnny-five');
-const board = new five.Board({
-  io: new raspi()
-});
+// const raspi = require('raspi-io');
+// const FormData = require('form-data');
+// const five = require('johnny-five');
+// const board = new five.Board({
+//   io: new raspi()
+// });
 
-board.on("ready", function() {
+// board.on("ready", function() {
   
-  // Create a new `button` hardware instance.
-  var button = new five.Button({
-    pin: "P1-7",
-    isPullup: true
-  });
+//   // Create a new `button` hardware instance.
+//   var button = new five.Button({
+//     pin: "P1-7",
+//     isPullup: true
+//   });
   
-  button.on("hold", function() {
-    console.log( "Button held" );
-  });
+//   button.on("hold", function() {
+//     console.log( "Button held" );
+//   });
   
-  button.on("press", function() {
-    console.log( "Button pressed" );
-  });
+//   button.on("press", function() {
+//     console.log( "Button pressed" );
+//   });
   
-  button.on("release", function() {
-    const stillCamera = new StillCamera({
-      width: 864,
-      height: 648
-    });
-    stillCamera.takeImage().then(image => {
-      fs.writeFileSync('event.jpg', image, {flag:'w'}); 
+//   button.on("release", function() {
+//     const stillCamera = new StillCamera({
+//       width: 864,
+//       height: 648
+//     });
+//     stillCamera.takeImage().then(image => {
+//       fs.writeFileSync('event.jpg', image, {flag:'w'}); 
 
-    const form = new FormData();
-    form.append('event', fs.createReadStream('event.jpg'));
-    fetch('http://172.20.10.6:5000/api/v1/events', { method: 'POST', body: form, headers: form.getHeaders() })
-        .then(res => res.json())
-        .then(json => console.log(json));
-        }).catch(error => {
-          console.log(error.message);
-        })
-    console.log( "Button released" );
-  });
-});
+//     const form = new FormData();
+//     form.append('event', fs.createReadStream('event.jpg'));
+//     fetch('http://172.20.10.6:5000/api/v1/events', { method: 'POST', body: form, headers: form.getHeaders() })
+//         .then(res => res.json())
+//         .then(json => console.log(json));
+//         }).catch(error => {
+//           console.log(error.message);
+//         })
+//     console.log( "Button released" );
+//   });
+// });
---
+++ onOff.js
@@ -0,0 +1,38 @@
+const { StillCamera } = require('pi-camera-connect');
+const stillCamera = new StillCamera();
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const raspi = require('raspi-io');
+const FormData = require('form-data');
+
+const Gpio = require('onoff').Gpio;
+
+let button = new Gpio(4, 'in', 'both');
+
+button.watch((err, value) => {
+  if(err) {
+    throw err;
+  }
+  button.writeSync(value);
+});
+
+process.on('SIGINT', () => {
+  button.unexport();
+  const stillCamera = new StillCamera({
+    width: 864,
+    height: 648
+  });
+  stillCamera.takeImage().then(image => {
+    fs.writeFileSync('event.jpg', image, {flag:'w'}); 
+
+  const form = new FormData();
+  form.append('event', fs.createReadStream('event.jpg'));
+  fetch('http://172.20.10.6:5000/api/v1/events', { method: 'POST', body: form, headers: form.getHeaders() })
+      .then(res => res.json())
+      .then(json => console.log(json));
+      }).catch(error => {
+        console.log(error.message);
+      })
+  console.log( "Button released" );
+});
