ebd946c1368a182d6d9d810a2301f413e7fb2913
Hoa Luu <hoamluu@gmail.com>
2018-08-29 16:51:32 -0400

    isTakingPicture

--- onOff.js
+++ onOff.js
@@ -1,4 +1,6 @@
-const { StillCamera } = require('pi-camera-connect');
+const {
+  StillCamera
+} = require('pi-camera-connect');
 const stillCamera = new StillCamera();
 const fs = require('fs');
 const fetch = require('node-fetch');
@@ -9,27 +11,45 @@ const FormData = require('form-data');
 const Gpio = require('onoff').Gpio;
 
 let button = new Gpio(4, 'in', 'both');
-
+let isTakingPicture = false;
 button.watch((err, value) => {
-  if(err) {
-    throw err;
+  if (err) {
+    console.log(err);
+    
   }
-  const stillCamera = new StillCamera({
-    width: 864,
-    height: 648
-  });
-  stillCamera.takeImage().then(image => {
-    fs.writeFileSync('event.jpg', image, {flag:'w'}); 
+  if (isTakingPicture === false) {
+    isTakingPicture = true;
+    const stillCamera = new StillCamera({
+      width: 864,
+      height: 648
+    });
+    stillCamera.takeImage().then(image => {
+      fs.writeFileSync('event.jpg', image, {
+        flag: 'w'
+      });
 
-  const form = new FormData();
-  form.append('event', fs.createReadStream('event.jpg'));
-  fetch('http://172.20.10.6:5000/api/v1/events', { method: 'POST', body: form, headers: form.getHeaders() })
-      .then(res => res.json())
-      .then(json => console.log(json));
-      }).catch(error => {
-        console.log(error.message);
-      })
-  console.log( "Button released" );
+      const form = new FormData();
+      form.append('event', fs.createReadStream('event.jpg'));
+      fetch('http://172.20.10.6:5000/api/v1/events', {
+          method: 'POST',
+          body: form,
+          headers: form.getHeaders()
+        })
+        .then(res => res.json())
+        .then(json => {
+          console.log(json);
+          isTakingPicture = false;
+        }).catch(error => {
+          console.log(error.message);
+          isTakingPicture = false;
+        })
+
+    });
+    console.log("Button released");
+  } else {
+    console.log("processing image please wait");
+    
+  }
 });
 
 process.on('SIGINT', () => {
